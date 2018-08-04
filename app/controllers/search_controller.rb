8270356c82baaaae5c5ff6856584fbf3642fa9ef
MINKYU <korea2017180052@gmail.com>
2018-08-05 03:35:23 +0900

    temp

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,12 +2,15 @@ class SearchController < ApplicationController
   helper_method :titleOverlap?
 
   def index
-    
-    @url = RestClient.get("http://ebook.starlove.net/Home/Index?libcode=99&KeyField=name&Keyword=%ED%8D%BC%ED%8E%99%ED%8A%B8&page=1")
+    @title=params[:title]
+    require 'uri'
+    @url = RestClient.get("http://ebook.starlove.net/Home/Index?libcode=99&KeyField=name&Keyword=#{URI::encode(@title)}&page=1")
     @result = Nokogiri::HTML.parse(@url)
     # nokogiri를 이용하여 원하는 부분을 골라냄
     @books = @result.css('div>table.ebookList')
     @i = 1
     @bookArr = []
+  end
+  def search 
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root 'search#index'
-
+  root 'search#search'
+  get 'search/search'
+  get 'search/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

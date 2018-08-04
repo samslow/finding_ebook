c52efa358a4dc5ea5b2ffa81d9e8b9fcc1f4a1dd
samslow <slow@samslowui-MacBook-Pro.local>
2018-08-04 23:21:04 +0900

    I want go home..

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,8 +4,7 @@ class SearchController < ApplicationController
     @result = Nokogiri::HTML.parse(@url)
     # nokogiri를 이용하여 원하는 부분을 골라냄
     @books = @result.css('div>table.ebookList')
-    # 책의 검색 결과
-    # @books = @search.children.children.children.children[5].text
-
+    @i = 1
+    
   end
 end

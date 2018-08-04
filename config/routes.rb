60b9b7593a19d273bed2471e20342d641ed203c7
samslow <slow@samslowui-MacBook-Pro.local>
2018-08-05 05:35:37 +0900

    i want go home... please:

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root 'search#search'
-  get 'search/search'
   get 'search/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
